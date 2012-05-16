b507abfeb881e5e3cd7d2bda3dbec7e8ab46809e
Mike <mike@jarvis.chu.cam.ac.uk>
2012-05-16 16:15:20 +0100

    modifications to remove reliance on nrntools

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ email:mv333@cam.ac.uk
 """
 
 from neuron import h
-from nrndev import nrntools
+import neuron
+import numpy as np
+from matplotlib import pyplot as plt
+#from nrndev import nrntools
 
 
 muscle=h.Section()
@@ -27,8 +30,36 @@ muscle.insert('kv')
 muscle.insert('canrgc')
 muscle.insert('cad2')
 
-sim=nrntools.Simulation(muscle,sim_time=2200,v_init=-70.0)
-sim.set_IClamp(100, 0.2, 2000)
-sim.go()
+stim=h.IClamp(muscle(0.5))
+stim.delay=100
+stim.amp=0.2
+stim.dur=200
 
-sim.show()
+#set the recroding section:
+rec_t=h.Vector()
+rec_t.record(h._ref_t)
+rec_v=h.Vector()
+rec_v.record(muscle(0.5)._ref_v)
+
+
+h.dt=0.05
+h.finitialize(-70.0)
+neuron.init()
+sim_time=2000
+
+if sim_time:
+    neuron.run(sim_time)
+else:
+    neuron.run(sim_time)
+
+
+x=np.array(rec_t)
+y=np.array(rec_v)
+plt.plot(x,y)
+plt.show()
+
+##if using nrntools, much of the above can be accomplished as follows:
+#sim=nrntools.Simulation(muscle,sim_time=2200,v_init=-70.0)
+#sim.set_IClamp(100, 0.2, 2000)
+#sim.go()
+#sim.show()
