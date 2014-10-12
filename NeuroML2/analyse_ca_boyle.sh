63ea948fe65d8835a5966e33e47ca4d7764f2c32
Padraig Gleeson <p.gleeson@gmail.com>
2014-10-12 18:04:00 +0100

    Updated .travis file try running generated files
    Note: omt files removed, since these are used normally for committed files, not generated files. Tests in travis now cover these

--- NeuroML2/analyse_ca_boyle.sh
+++ NeuroML2/analyse_ca_boyle.sh
@@ -2,4 +2,4 @@
 
 set -e
 python ~/git/BlueBrainProjectShowcase/Channelpedia/NML2ChannelAnalyse.py -temperature 34   -minV -55  -maxV 80  -duration 600  -clampBaseVoltage -55  -clampDuration 580  -stepTargetVoltage 10  -erev 50  -caConc 0.001 ca_boyle.channel.nml ca_boyle
-jnml LEMS_Test_ca_boyle.xml
+jnml LEMS_Test_ca_boyle.xml $1
--- NeuroML2/analyse_k_slow.sh
+++ NeuroML2/analyse_k_slow.sh
@@ -2,4 +2,4 @@
 
 set -e
 python ~/git/BlueBrainProjectShowcase/Channelpedia/NML2ChannelAnalyse.py -temperature 34   -minV -55  -maxV 80  -duration 600  -clampBaseVoltage -55  -clampDuration 580  -stepTargetVoltage 10  -erev -55  k_slow.channel.nml k_slow
-jnml LEMS_Test_k_slow.xml
+jnml LEMS_Test_k_slow.xml $1
