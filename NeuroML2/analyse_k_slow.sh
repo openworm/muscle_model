25458736c870476fca60de64c3f78c1c2ed58eaf
Richard C Gerkin <rgerkin@asu.edu>
2015-01-17 14:27:03 -0700

    Removed path hard coding in favor of optional OPENWORM_HOME in accordance with issue #25

--- NeuroML2/analyse_k_slow.sh
+++ NeuroML2/analyse_k_slow.sh
@@ -1,5 +1,6 @@
 # Assumes https://github.com/OpenSourceBrain/BlueBrainProjectShowcase has been cloned in ~/git
 
 set -e
-python ~/git/BlueBrainProjectShowcase/Channelpedia/NML2ChannelAnalyse.py -temperature 34   -minV -55  -maxV 80  -duration 600  -clampBaseVoltage -55  -clampDuration 580  -stepTargetVoltage 10  -erev -55  k_slow.channel.nml k_slow
+OPENWORM_HOME=${OPENWORM_HOME:-~/git/}
+python $OPENWORM_HOME/BlueBrainProjectShowcase/Channelpedia/NML2ChannelAnalyse.py -temperature 34   -minV -55  -maxV 80  -duration 600  -clampBaseVoltage -55  -clampDuration 580  -stepTargetVoltage 10  -erev -55  k_slow.channel.nml k_slow
 jnml LEMS_Test_k_slow.xml $1
