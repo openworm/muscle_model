55c513a036bbf64698736982693b9ba6cd6ad71e
Vahid Ghayoomi <vahidghayoomi@gmail.com>
2015-05-18 23:16:14 +0430

    Update analyse_k_slow.sh

--- NeuroML2/analyse_k_slow.sh
+++ NeuroML2/analyse_k_slow.sh
@@ -1,4 +1,3 @@
-# Assumes https://github.com/OpenSourceBrain/BlueBrainProjectShowcase has been cloned in ~/git
 
 set -e
 pynml-channelanalysis -temperature 34   -minV -55  -maxV 80  -duration 600  -clampBaseVoltage -55  -clampDuration 580  -stepTargetVoltage 10  -erev -55 -norun k_slow.channel.nml
