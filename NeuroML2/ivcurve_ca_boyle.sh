e0cedf7bd8cfd8bdee1a4835636404ca34da6f63
Padraig Gleeson <p.gleeson@gmail.com>
2015-08-24 15:30:48 +0100

    Adding script to generate iv curve for ca_boyle channel

---
+++ NeuroML2/ivcurve_ca_boyle.sh
@@ -0,0 +1,1 @@
+pynml-channelanalysis -minV -40 -maxV 80  -duration 2000  -clampBaseVoltage -55 -clampDelay 50 -clampDuration 2000  -stepTargetVoltage 5  -erev 49.11  -caConc 0.001 -ivCurve ca_boyle.channel.nml $1
