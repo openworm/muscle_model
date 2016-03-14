51c79a18b7b1bf529dbc749cbf2019b5c4470d5a
Padraig Gleeson <p.gleeson@gmail.com>
2016-03-14 10:36:06 +0000

    Removing unnecessary test.sh

--- NeuroML2/test.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-jnml -validate k*channel.nml Leak.channel.nml CaPool.nml *.cell.nml Figure2A.net.nml
-jnml LEMS_Figure2A.xml -nogui
-jnml LEMS_Figure2B.xml -nogui
-
-./analyse_k_slow.sh -nogui
-./analyse_k_fast.sh -nogui
-./analyse_ca_boyle.sh -nogui
